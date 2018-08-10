517f13239a6aa02de0e052f10a1a521cbb5d314c
Shintaro Umezawa <shin.sk8.216@gmail.com>
2018-08-11 00:51:17 +0900

    Create assessment.js

---
+++ assessment.js
@@ -0,0 +1,89 @@
+(function (){
+  'use strict';
+  const userNameInput     = document.getElementById('user-name');
+  const assessmentButton  = document.getElementById('assessment');
+  const resultDivided     = document.getElementById('tweet-area');
+
+  /**
+  *指定した要素の子どもを全て除去する
+  *@param {HTMLElement} element HTML要素
+  */
+    function removeAllChildren(element){
+        while (element.firstChild){//子どもの要素が有る限り削除
+            element.removeChile(element.firstChild);
+          }
+    }
+    assessmentButton.onclick = () => {
+      const userNameInput   = userName.Input.value;
+      if (userName.length === 0){//名前が空の時は処理を終了する
+          return;
+      }
+
+    //診断結果表示エリアの作成
+    removeAllChildren(resultDivided);
+    const header        = document.createElement('h3');
+    header.innerText    = '診断結果';
+    resultDivided.appendChild(header);
+
+    const paragraph     = document.createElement('p');
+    const result        = assessment(userName);
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
+
+    //ツイートエリアの作成
+    removeAllChildern(tweetDivided);
+    const anchor        = document.createElement('a');
+    const hrefValue     = '' + encodeURIcomponent(result);
+    anchor.setAttribute('href', hrefValue);
+    anchor.className    = 'twitter-hashtag-button';
+    anchor.innerText    = '';
+    tweetDivided.appendChild(anchor);
+
+    twttr.widgets.load();
+    };
+
+    userNameInput.onkeydown = (event) => {
+      if (event.keyCode === 13){
+        assessmentButton.onclick();
+      }
+    };
+
+    const answer = [
+    '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+    '{userName}のいいところは眼差しです。{userName}に見つめられた人は、気になって仕方ないでしょう。',
+    '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます',
+    '{userName}のいいところは厳しさです。{userName}の厳しさがいつも成功に導きます',
+    '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています',
+    '{userName}のいいところはユニークさです。{userName}だけのその特徴がみんなを楽しくします。'
+    ];
+
+    /*
+    *名前の文字列を渡すと診断結果を返す関数
+    *@param {string} userName ユーザの名前
+    *@return {string} 診断結果
+    */
+    function assessment(userName){
+      //全文字のコード番号を取得してそれを足し合わせる
+      let sumOfcharCode = 0;
+      for (let i = 0; i < userName.length; i++){
+        sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+      }
+
+      //文字のコード番号の合計を回数の数で割って添字の数値を求める
+      const index   = sumOfcharCode % answers.length;
+      let result    = answers[index];
+
+      result        = result.replace(/{userName}/g, userName);
+      return result;
+    }
+
+    //テストコード
+    console.assert(
+      assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+      '診断結果の文言の特定の部分を名前に書き換える処理が正しくありません。'
+      );
+    console.assert(
+      assessment('太郎') === assessment('太郎'),
+      '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+      );
+  })();
